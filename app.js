2a83f5743c89933af79f87cd77c69e0ff51f1f63
Pirci <emrepirci@gmail.com>
2021-12-17 17:54:44 +0100

    Javascript logic

--- app.js
+++ app.js
@@ -3,13 +3,13 @@ const generateBtn = document.querySelector('.generateBtn');
 generateBtn.addEventListener('click', generateRandomNum);
 
 function generateRandomNum() {
-  const fromNum = document.querySelector('.fromNum').value;
-  const toNum = document.querySelector('.toNum').value;
+	const fromNum = document.querySelector('.fromNum').value;
+	const toNum = document.querySelector('.toNum').value;
 
-  randomNum = Math.floor(Math.random() * parseInt(toNum));
+	randomNum = Math.floor(Math.random() * parseInt(toNum));
 
-  if (randomNum >= parseInt(fromNum)) {
-    const randomNumEl = document.querySelector('.randomNumEl');
-    randomNumEl.innerHTML = randomNum;
-  }
+	if (randomNum >= parseInt(fromNum)) {
+		const randomNumEl = document.querySelector('.randomNumEl');
+		randomNumEl.innerHTML = randomNum;
+	}
 }
