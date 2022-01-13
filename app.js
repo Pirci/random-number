e573b3ea4d857a29a6fa89050948af0de2fa4fc2
Pirci <emrepirci@gmail.com>
2022-01-13 14:49:15 +0100

    Add code comments

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+// Create variable and DOM
 const generateBtn = document.querySelector('.generateBtn');
 
+// Button click event listener
 generateBtn.addEventListener('click', generateRandomNum);
 
 function generateRandomNum() {
 	const fromNum = document.querySelector('.fromNum').value;
 	const toNum = document.querySelector('.toNum').value;
 
+	// Create number number between given numbers
 	randomNum = Math.floor(Math.random() * parseInt(toNum));
 
 	if (randomNum >= parseInt(fromNum)) {
